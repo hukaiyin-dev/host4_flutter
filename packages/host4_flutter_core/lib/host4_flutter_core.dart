b9aafcc0ef3a2a03c8032e87497c72b24dc216b7
wl <18380173957@163.com>
2026-05-27 18:03:40 +0800

    feat: 暴露网络请求client文件.

--- packages/host4_flutter_core/lib/host4_flutter_core.dart
+++ packages/host4_flutter_core/lib/host4_flutter_core.dart
@@ -3,3 +3,5 @@ export 'src/log/host4_log_level.dart';
 export 'src/log/host4_log_sink.dart';
 export 'src/module/host4_module.dart';
 export 'src/runtime/host4_environment.dart';
+export 'src/net/dio_client.dart';
+
