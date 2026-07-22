3554f766ef07cda7826025c09efbbe1e72de6dd4
jingpan <791676149@qq.com>
2026-07-22 11:31:03 +0800

    加固IOS BLE OTA重复收到固件数据流程

---
+++ examples/host4_flutter_demo/test/gmacro_ota_firmware_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:host4_flutter_demo/pages/gmacro/gmacro_session_page.dart';
+import 'package:host4_flutter_transport/host4_flutter_transport.dart';
+
+void main() {
+  test('selects BLE bundled OTA firmware for BLE transport', () {
+    final firmware = gmacroBundledOtaFirmwareForTransportKind(
+      TransportKind.ble,
+    );
+
+    expect(
+      firmware.assetPath,
+      'assets/ota/OTA_GDF-G560637_46D4_V1.0_260510a.bin',
+    );
+    expect(firmware.fileName, 'OTA_GDF-G560637_46D4_V1.0_260510a.bin');
+  });
+
+  test('selects MFi bundled OTA firmware for MFi transport', () {
+    final firmware = gmacroBundledOtaFirmwareForTransportKind(
+      TransportKind.mfi,
+    );
+
+    expect(
+      firmware.assetPath,
+      'assets/ota/OTA_GDF-G910202_8520_V1.0_260715a.bin',
+    );
+    expect(firmware.fileName, 'OTA_GDF-G910202_8520_V1.0_260715a.bin');
+  });
+}
